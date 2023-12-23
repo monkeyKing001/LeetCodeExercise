85ec014d41fbef7172035fb436976f266f0ae07d
monkeyKing <67903821+monkeyKing001@users.noreply.github.com>
2023-12-23 16:34:51 +0900

    Time: 420 ms (50.81%), Space: 0B (100.00%) - LeetHub

--- LeetCode/Easy/1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
+++ LeetCode/Easy/1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -1,15 +1,61 @@
 # Write your MySQL query statement below
 SELECT
-    a1.machine_id, ROUND(AVG(a2.timestamp - a1.timestamp), 3) AS processing_time
-    #a1.machine_id, a1,timestamp, a2.machine_id, a2.timestamp
+    A1.machine_id, ROUND(AVG(A2.timestamp - A1.timestamp), 3) as processing_time
 FROM
-    Activity AS a1,
-    Activity AS a2
-WHERE 
-    a1.machine_id = a2.machine_id
-AND
-    a1.process_id = a2.process_id
-AND
-    a1.activity_type = 'start' AND a2.activity_type = 'end'
+    Activity A1
+INNER JOIN
+    Activity A2
+ON 
+    A1.machine_id = A2.machine_id 
+    AND A1.process_id = A2.process_id 
+    AND A1.activity_type = 'start'
+    AND A2.activity_type = 'end'
 GROUP BY
-    a1.machine_id;
+    A1.machine_id
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#SELECT
+#    a1.machine_id, ROUND(AVG(a2.timestamp - a1.timestamp), 3) AS processing_time
+#    #a1.machine_id, a1,timestamp, a2.machine_id, a2.timestamp
+#FROM
+#    Activity AS a1,
+#    Activity AS a2
+#WHERE 
+#    a1.machine_id = a2.machine_id
+#AND
+#    a1.process_id = a2.process_id
+#AND
+#    a1.activity_type = 'start' AND a2.activity_type = 'end'
+#GROUP BY
+#    a1.machine_id;
