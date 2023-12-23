f69c9b6f3d808527c9a71112f7d20fe1c562cc1f
monkeyKing <67903821+monkeyKing001@users.noreply.github.com>
2023-12-23 15:44:32 +0900

    Time: 638 ms (95.55%), Space: 0B (100.00%) - LeetHub

--- LeetCode/Easy/1148-article-views-i/1148-article-views-i.sql
+++ LeetCode/Easy/1148-article-views-i/1148-article-views-i.sql
@@ -1,9 +1,10 @@
 # Write your MySQL query statement below
 SELECT
-    DISTINCT(author_id) AS id
+    DISTINCT(author_id) as id
 FROM
     Views
 WHERE
     author_id = viewer_id
 ORDER BY
-    author_id ASC;
+    id ASC;
+   
