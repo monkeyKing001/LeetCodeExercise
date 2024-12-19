57f8ef4dd170ef246ea998f1d95c804cbaa39ce0
Dong Woo Kwak <67903821+monkeyKing001@users.noreply.github.com>
2024-12-19 13:25:44 +0900

    Time: 0 ms (100%), Space: 14.2 MB (18.8%) - LeetHub

--- 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -2,23 +2,14 @@ class Solution {
 public:
   vector<int> finalPrices(vector<int>& prices) {
     vector<int> ans(prices.begin(), prices.end());
-    vector<int> dp(prices.begin(), prices.end());
-    for (int i = dp.size() - 1; i >= 0; i--){
-      if (i != dp.size() - 1){
-        dp[i] = min(dp[i], dp[i + 1]);
-      }
-    }
-    for (int i = 0; i < ans.size(); i++){
-      if (ans[i] >= dp[i]){
-        int l = i + 1;
-        while (l < ans.size()){
-          if (ans[l] <= ans[i]){
-            ans[i] -= ans[l];
-            break;
-          }
-          l++;
-        }
-      }
+    stack<int> s;
+    int n = prices.size();
+    for (int i = n - 1; i >= 0; i--){
+      while (!s.empty() && prices[s.top()] > prices[i])
+        s.pop();
+      if (s.size() != 0)
+        ans[i] -= prices[s.top()];
+      s.push(i);
     }
     return (ans);
   }
