249c241ea2c2db6c904f44d24a6bc79f02490755
Dong Woo Kwak <67903821+monkeyKing001@users.noreply.github.com>
2025-01-10 17:27:48 +0900

    Time: 45 ms (70.78%), Space: 104.6 MB (68.49%) - LeetHub

--- 0952-word-subsets/0952-word-subsets.cpp
+++ 0952-word-subsets/0952-word-subsets.cpp
@@ -6,10 +6,10 @@ public:
     vector<int> w2_count(26, 0);
     for (auto &w2 : words2){
       vector<int> temp(26, 0);
-      for (auto w2c : w2){
+      for (auto w2c : w2)
         temp[w2c - 'a']++;
-        w2_count[w2c - 'a'] = max(w2_count[w2c - 'a'], temp[w2c - 'a']);
-      }
+      for (int i = 0; i < 26; i++)
+        w2_count[i] = max(w2_count[i], temp[i]);
     }
     for (auto &w1 : words1){
       vector<int> ww1_count(26, 0);
@@ -18,11 +18,10 @@ public:
       }
       bool flag = true;
       for (int i = 0; i < 26 && flag; i++){
-        if (w2_count[i] && w2_count[i] > ww1_count[i])
+        if (w2_count[i] > ww1_count[i])
           flag = false;
       }
-      if (flag)
-        ans.push_back(w1);
+      if (flag) ans.push_back(w1);
     }
     return (ans);
   }
