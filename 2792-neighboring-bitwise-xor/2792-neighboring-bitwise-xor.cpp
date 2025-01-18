5dccd0f7f252666945a9d43c2fd92ffe683afd52
Dong Woo Kwak <67903821+monkeyKing001@users.noreply.github.com>
2025-01-18 18:24:31 +0900

    Time: 4 ms (50%), Space: 263.8 MB (77.22%) - LeetHub

--- 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.cpp
+++ 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
-    static bool doesValidArrayExist(vector<int>& derived) {
-        return accumulate(derived.begin(), derived.end(), 0, bit_xor<>())==0;
+  bool doesValidArrayExist(vector<int>& derived) {
+    int n = derived.size();
+    bool start = true;
+    bool cur = start;
+    for (int i = 0; i < n - 1; i++){
+      if (derived[i])
+        cur = (cur == 0);
     }
+    if (cur == start && derived.back() == 1)
+      return false;
+    if (cur != start && derived.back() == 0)
+      return false;
+    return (true);
+  }
 };
