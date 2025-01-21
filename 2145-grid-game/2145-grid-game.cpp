8977f6e19c33cfd3656174bc2564c2d72a83418e
Dong Woo Kwak <67903821+monkeyKing001@users.noreply.github.com>
2025-01-21 18:45:48 +0900

    Time: 0 ms (100%), Space: 66.7 MB (61.54%) - LeetHub

--- 2145-grid-game/2145-grid-game.cpp
+++ 2145-grid-game/2145-grid-game.cpp
@@ -1,28 +1,18 @@
-#define ll long long
+
 class Solution {
 public:
-  long long gridGame(vector<vector<int>>& grid) {
-    int n = grid.size();
-    int m = grid[0].size();
-    ll ans = 1e10;
-    vector<vector<ll>> prefix_sum(n, vector<ll>(m, 0));
-    vector<vector<ll>> next_prefix_sum(n, vector<ll>(m, 0));
-    for (int i = 0; i < m; i++){
-      int ii = m - 1 - i;
-      prefix_sum[0][i] += grid[0][i];
-      prefix_sum[1][ii] += grid[1][ii];
-      if (i > 0){
-        prefix_sum[0][i] += prefix_sum[0][i - 1];
-        prefix_sum[1][ii] += prefix_sum[1][ii + 1];
-      }
+    long long gridGame(vector<vector<int>>& grid) {
+        long long top = 0;
+        for (int i = 0; i < grid[0].size(); i++) {
+            top += grid[0][i];
+        }
+
+        long long bottom = 0, res = LLONG_MAX;
+        for (int i = 0; i < grid[0].size(); i++) {
+            top -= grid[0][i]; // First robot picks from the top row 
+            res = min(res, max(top, bottom)); // Minimize the maximum score for the second robot
+            bottom += grid[1][i]; // Second robot picks from the bottom row \U0001f916
+        }
+        return res; 
     }
-    
-    for (int i = 0; i < m; i++){
-      ll up_rest = prefix_sum[0].back() - prefix_sum[0][i];
-      ll down_rest = prefix_sum[1][0] - prefix_sum[1][i];
-      ll temp_blue_score = max(up_rest, down_rest);
-      ans = min(ans, temp_blue_score);
-    }
-    return (ans);
-  }
 };
