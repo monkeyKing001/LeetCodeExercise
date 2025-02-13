f16f600b992dcaf79d5fb2a69b0aa838a190737f
Dong Woo Kwak <67903821+monkeyKing001@users.noreply.github.com>
2025-02-13 16:37:00 +0900

    Time: 871 ms (83.45%), Space: 0B (100%) - LeetHub

--- 2041-the-latest-login-in-2020/2041-the-latest-login-in-2020.sql
+++ 2041-the-latest-login-in-2020/2041-the-latest-login-in-2020.sql
@@ -1,13 +1,5 @@
 # Write your MySQL query statement below
-SELECT L.user_id, L.time_stamp AS 'last_stamp'
+SELECT L.user_id, MAX(L.time_stamp) AS 'last_stamp'
 FROM Logins L
-WHERE DATEDIFF(L.time_stamp, '2020-01-01') >= 0 
-    AND DATEDIFF(L.time_stamp, '2021-01-01') < 0
-    AND (
-        SELECT COUNT(*)
-        FROM Logins LL
-        WHERE LL.user_id = L.user_id
-        AND L.time_stamp < LL.time_stamp
-        AND DATEDIFF(LL.time_stamp, '2020-01-01') >= 0 
-        AND DATEDIFF(LL.time_stamp, '2021-01-01') < 0
-    ) = 0
+WHERE L.time_stamp LIKE '2020%' 
+GROUP BY L.user_id
