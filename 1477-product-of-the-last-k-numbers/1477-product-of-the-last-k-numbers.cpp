17aa0259d92f2e7b90200d449516088aa90afbae
Dong Woo Kwak <67903821+monkeyKing001@users.noreply.github.com>
2025-02-14 10:21:18 +0900

    Time: 19 ms (73.09%), Space: 77.9 MB (93.67%) - LeetHub

--- 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.cpp
+++ 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.cpp
@@ -1,45 +1,19 @@
 class ProductOfNumbers {
+private:
+  vector<int> prefix_x;
 public:
-    vector<int> prefix_product;
-    set<int> zero_idx;
-
     ProductOfNumbers() {
-      prefix_product.resize(0, 0);
+      prefix_x.resize(1, 1);
     }
     
     void add(int num) {
-      prefix_product.push_back(num);
-      if (num){
-        if (prefix_product.size() >= 2 && \
-            prefix_product[prefix_product.size() - 2]){
-            prefix_product[prefix_product.size() - 1] *= prefix_product[prefix_product.size() - 2];
-        }
-      }
-      else zero_idx.insert(prefix_product.size() - 1);
+      if (num) prefix_x.push_back(prefix_x.back() * num);
+      else prefix_x.resize(1, 1);
     }
     
     int getProduct(int k) {
-      //for (int i = 0; i < prefix_product.size(); i++){
-      //  cout << prefix_product[i] << " ";
-      //}
-      //cout << "\n";
-      int last_idx = prefix_product.size() - 1;
-      int exclude_idx = last_idx - k;
-      auto zero_it = zero_idx.upper_bound(exclude_idx);
-      if (zero_it == zero_idx.end() || *zero_it > last_idx){
-        if (exclude_idx == -1 || !prefix_product[exclude_idx]) 
-          return (prefix_product[last_idx]);
-        else
-          return (prefix_product[last_idx] / prefix_product[exclude_idx]);
-      }
-      else{
-        //cout
-        //  << "exclude idx, zero_idx  : "
-        //  << exclude_idx
-        //  << ", "
-        //  << *zero_it << "\n";
-        return (0);
-      }
+      if (k >= prefix_x.size() || prefix_x.size() == 1) return (0);
+      else return (prefix_x.back() / prefix_x[prefix_x.size() - k - 1]);
     }
 };
 
