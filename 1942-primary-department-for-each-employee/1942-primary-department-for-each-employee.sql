c4b39f4e56bfa7a54cc5c32992a1fa3259a4954c
Dong Woo Kwak <67903821+monkeyKing001@users.noreply.github.com>
2025-02-17 20:24:01 +0900

    Time: 848 ms (69.84%), Space: 0B (100%) - LeetHub

--- 1942-primary-department-for-each-employee/1942-primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/1942-primary-department-for-each-employee.sql
@@ -1,9 +1,10 @@
 # Write your MySQL query statement below
 SELECT E.employee_id, E.department_id
 FROM Employee E
-WHERE E.primary_flag = 'Y' OR employee_id IN (
-    SELECT EE.employee_id 
+WHERE E.primary_flag = 'Y' OR E.employee_id IN 
+(
+    SELECT EE.employee_id
     FROM Employee EE
     GROUP BY EE.employee_id
-    HAVING (COUNT(EE.employee_id) = 1)
-    )
+    HAVING COUNT(EE.department_id) = 1
+)
