7c17ad9cbb7a6c5278d7ac92ea8248ea107d8116
Dong Woo Kwak <67903821+monkeyKing001@users.noreply.github.com>
2025-02-19 15:02:08 +0900

    Time: 866 ms (26.9%), Space: 0B (100%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,8 +1,10 @@
 # Write your MySQL query statement below
-SELECT 
-    (SELECT num
-     FROM MyNumbers
-     GROUP BY num
-     HAVING COUNT(num) = 1
-     ORDER BY num DESC
-     LIMIT 1) AS num;
+SELECT MAX(MM.num) AS num
+FROM (
+    SELECT M.num
+    FROM MyNumbers M
+    GROUP BY M.num
+    HAVING COUNT(num) = 1
+    ORDER BY num DESC
+    LIMIT 1
+) as MM
