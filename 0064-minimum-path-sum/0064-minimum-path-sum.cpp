ccd3cb2eff37ff3ce893ec3e5b4431a84e0f485b
Dong Woo Kwak <67903821+monkeyKing001@users.noreply.github.com>
2025-03-02 18:45:46 +0900

    Time: 2 ms (35.65%), Space: 16.2 MB (56.77%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -23,8 +23,18 @@ public:
   int minPathSum(vector<vector<int>>& grid) {
     m = grid.size();
     n = grid[0].size();
-    vector<vector<int>> dp(m, vector<int>(n, -1));
-    int ans = rec(grid, dp, {0, 0});
-    return (ans);
+    vector<vector<int>> dp(m, vector<int>(n, 1 << 20));
+    dp[m - 1][n - 1] = grid[m - 1][n - 1];
+    //vector<vector<int>> dp = grid;
+    for (int i = m - 1; i >= 0; i--){
+      for (int j = n - 1; j >= 0; j--){
+        if (i > 0)
+          dp[i - 1][j] = min(dp[i - 1][j], dp[i][j] + grid[i - 1][j]);
+        if (j > 0)
+          dp[i][j - 1] = min(dp[i][j - 1], dp[i][j] + grid[i][j - 1]);
+      }
+    }
+    //int ans = rec(grid, dp, {0, 0});
+    return (dp[0][0]);
   }
 };
